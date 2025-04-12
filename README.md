2ccc72de956cc9f66c0509a18ac63fff63fce09d
Kishan Kumar <kishankr1107@gmail.com>
2025-04-12 19:06:18 +0530

    Episode 2

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Namaste Javascript 💥
+
+# parcel
+-dev vuild
+-Local Server
+-HMR= Hot Module Replacement 
+-File watching algorithm- written in c++
+-Caching- faster builds
+-Image Optimisation
+-Minification
+-Bundling
+-Compress
+-Consistent hashing
+-differntial bundling
+-Error Handling
+-Host your app on HTTPS
+-Tree shaking alogorithm - remove unuse code 
+
--- app.js
+++ app.js
@@ -1,18 +1,22 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+
 const parent = React.createElement(
     "div", 
     {id:"parent"},[
-    React.createElement("div",{id:"child"},[
-        React.createElement("h1",{},"I am a h1 Tag"),
-        React.createElement("h2",{},"I am a h2 Tag")
+    React.createElement("div",{id:"child",key:"child1"},[
+        React.createElement("h1",{key:"h11"},"I am a h1 Tag"),
+        React.createElement("h2",{key:"h21"},"I am a h2 Tag")
     ]),
-    React.createElement("div",{id:"child2"},[
-        React.createElement("h1",{},"I am a h1 Tag"),
-        React.createElement("h2",{},"I am a h2 Tag")
+    React.createElement("div",{id:"child2",key:"child2"},[
+        React.createElement("h3",{key:"h12"},"I am a h1 Tag"),
+        React.createElement("h4",{key:"h22"},"I am a h2 Tag")
     ]),
     ]);
 
 console.log(parent);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 
 root.render(parent);
